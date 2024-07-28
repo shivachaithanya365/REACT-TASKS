364c617af94e143a8ef01441cf3135352ec0587d
shivachaithanya <shivachaithanya365@gmail.com>
2024-07-28 22:59:18 +0530

    updated

--- src/components/ProgressBar.js
+++
@@ -1,8 +0,0 @@
-import ProgressBar from 'react-bootstrap/ProgressBar';
-
-function CustomProgressBar(prop) {
-    const { scale } = prop;
-    return <ProgressBar animated now={scale} />;
-}
-
-export default CustomProgressBar;
