d712d1944d9ee87ae25225c0352c4093e0312700
shivachaithanya <shivachaithanya365@gmail.com>
2024-07-29 16:40:26 +0530

    Adding files of React/july-15

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ItemList from './components/Ternary-operator';
+import Ifelse from './components/If-else-condition';
+import ShortCircuit from './components/Short-circuit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1 className='App'>USING CLASS BASED COMPONENTS </h1>
+    <Ifelse />
+    <ItemList />
+    <ShortCircuit />
+  </div>
+);
 
 export default App;
